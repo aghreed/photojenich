9d26f70230aad186948bb275af67359c09801908
Whitney Olson Jenich <whitney@Whitneys-iMac.local>
2018-07-19 18:21:33 -0700

    text changes to Connect

--- src/pages/connect/index.js
+++ src/pages/connect/index.js
@@ -33,18 +33,17 @@ const QuestionsWeAskedText = (
     <p>2. What do you lie the most about?</p>
     <p>3. What are your beliefs on God?</p>
     <p>4. How long does it take you to really trust someone?</p>
-    <p>5. What is the craziest thing that you’ve done with someone?</p>
-    <p>6. What are you most grateful for in your life?</p>
-    <p>7. What type of relationship did you have with your parents?</p>
-    <p>8. What type of relationship did you have with your parents?</p>
-    <p>9. What emotion do you experience the most?</p>
+    <p>5. What is the craziest thing that you’ve done?</p>
+    <p>6. What are you most grateful for in life?</p>
+    <p>7. What's your relationship like with your parents?</p>
+    <p>8. What emotion do you experience the most?</p>
   </div>
 );
 
 const DigitalRealismText = (
   <div className="digital-realism-text-container">
     <p>
-      We questioned our participants about the use of technology in creating intimacy with two people to see if they could still make a meaningful connection through a screen.
+    We questioned our participants about the use of technology in creating intimacy with two people to see if they could still make a meaningful connection through a screen.
     </p>
     <p>
     Participant three stated,
@@ -64,31 +63,31 @@ const ConnectPage = ({
       image={ConnectBig}
       title="Connect"
       duration="1 Week"
-      role="Prototyping, Testing & Design"
+      role="Prototyping, Testing, & Design"
       projectColor={ConnectColor}
     />
     <ProjectDetailBrief
-      text="In recent studies it was stated that humans spend an average of five hours a day on their mobile devices (Cite) on major social media sites, games, entertainment, shopping, and more. While mobile phones essentially bring people closer together than ever before, we are left, somehow, feeling further apart."
-      text2="On top of that, we live in divisive times. Everyday feels like a shouting match, where no side is listening to the other. Technology is being used as a barrier to bully others you don’t even know. As a result, empathy for one other is at an all time low. Connect is an experimental voice activated phone charging station that aims to promote positive connection and empathy-building between two strangers through technology."
+      text="In a recent study published on Tech Crunch, it was stated that humans spend an average of five hours a day on their mobile devices––on major social media sites, games, entertainment, shopping, and more. While mobile phones essentially bring people closer together than ever before, we are left, somehow, feeling further apart."
+      text2="On top of that, we live in divisive times. Everyday feels like a shouting match, where no side is listening to the other. Technology is being used as a barrier to bully others you don’t even know. As a result, empathy for one other is at an all time low. Connect is an experimental, voice activated, phone charging station that aims to promote positive connection and empathy-building between two strangers through technology."
     />
     <VideoSection videoSrc="https://player.vimeo.com/video/259248269" />
     <SegwayBulletList
       projectColor={ConnectColor}
       projectBackgroundColor={ConnectBackgroundColor}
-      title="We Wanted to do Three Things"
+      title="Our Intention"
       itemOne="If only for a while, motivate individuals to take the device out of their hands."
       itemTwo="Talk to a stranger on a deep and personal level."
       itemThree="Remind individuals of the importance of empathy-building through the action of listening."
     />
     <SingleTitledParagraph
       title="How it Works"
-      text="Connect is an experimental charging station, activated by voice. While we use technology as a means for the two sides to connect, our goal was to get mobile devices out of the hands of the user and help facilitate a deep and meaningful connection with someone they may never see again.  We provide thought-provoking prompts to promote conversations and make it easier for the speaker to drop their guard. For the sake of our project we chose to have one speaker, and one listener to engage people in being both vulnerable and empathetic."
+      text="Connect is an experimental charging station, activated by voice. While we use technology as a means for the two sides to connect, our goal was to get mobile devices out of the hands of the user and help facilitate a deep and meaningful connection with someone they may never see again. We provide thought-provoking prompts to promote conversations and make it easier for the speaker to drop their guard. For the sake of our project we chose to have one speaker and one listener to engage in the roles of being vulnerable and empathetic."
     />
     <ParagraphImage
       goofy
       image={ConnectTheSpeaker}
       title="The Speaker"
-      text="The Speaker stands in front of our charging station, plugs in their phone, and takes a question card. Once they start answering a question aloud, their phone will begin to charge. From their view, they can see the back of the person watching them talk. We keep the listener anonymous through this experience to create a feeling of safety around sharing."
+      text="The Speaker stands in front of our charging station, plugs in their phone, and takes a question card. Once they start answering a question aloud, their phone will begin to charge. From their view, they can see the back of the person listening to them speak. We keep the listener anonymous through this experience to create a feeling of safety around sharing."
     />
     <ParagraphImage
       image={ConnectTheListener}
@@ -99,24 +98,24 @@ const ConnectPage = ({
       goofy
       image={ConnectWhatTheySee}
       title="What They Saw"
-      text="This is a view of what the Speaker sees when talking in front of the charging station. We wanted the Speaker to be able to see themselves talking to a thoughtful listener, while keeping the identity of that person a mystery. We wanted to see how a person would open up while talking to a complete stranger and how the person listening would feel after witnessing that moment."
+      text="This is a view of what the Speaker sees when talking in front of the charging station. We wanted the Speaker to be able to see themselves talking to a thoughtful listener, while keeping the identity of that person a mystery. We wished to see the experience of a person opening up while talking to a complete stranger and then reflect on how the Listener felt after witnessing that moment."
     />
     <ParagraphImage
       image={ConnectOurQuestions}
-      title="The Questions We Asked"
+      title="Our Questions"
       text={QuestionsWeAskedText}
     />
     <SegwayBulletList
       projectColor={ConnectColor}
       projectBackgroundColor={ConnectBackgroundColor}
       title="How We Got Here"
-      itemOne="Ideated four ideas, and rapidly paper prototyped for each idea."
+      itemOne="Ideated four ideas, and rapidly paper prototyped for each."
       itemTwo="Tested our prototypes with three participants to get a general understanding of what was working and what was not."
-      itemThree="Present our findings for a final critique that would help us synthesize down to one, final, idea."
+      itemThree="Presented our findings for a final critique that would help us synthesize down to one, final, idea."
     />
     <SingleTitledParagraph
       title="Putting Ideas to the Test"
-      text="Since we were interested in coming up with a way for people to interact by looking up from their phones, we brainstormed and came up with four different ideas that ranged from speculative art pieces to a simple way to individually manage time spent on their phone. We ended up combining the essence of two of our ideas to make our final Connect prototype."
+      text="Since we were interested in coming up with a way for people to interact by looking up from their phones, we brainstormed and came up with four different ideas that ranged from speculative art pieces, to a simple way to individually manage time spent on their phone. We ended up combining the essence of two of our concepts to make our final Connect prototype."
     />
     <ParagraphImage
       image={ConnectIcebreaker}
@@ -131,8 +130,7 @@ const ConnectPage = ({
     />
     <SingleTitledParagraph
       title="Feedback and Critique"
-      text="Our critique with our peers provided us with insightful comments that helped shape our final concept. They commentated that our “Power of Voice” prototype was less intrusive and would be accepted by more people. One classmate also suggested that instead of forcing people to talk to others standing nearby and avoid scenarios where someone was alone, it would be more natural for people to talk to someone on the screen. For example, a person can talk to another person who is at a different charging station through an iPad."
-      text2="Our peers also made comments on how they enjoyed that the “Ice Breaker” idea gave a prompt to break through the awkwardness of starting a conversation. These comments helped us shape the experience for our Speaker and Listener."
+      text="Our critique with our peers provided us with insightful comments that helped shape our final concept. They commentated that our “Power of Voice” prototype was less intrusive and would be accepted by more people. One classmate also suggested that instead of forcing people to talk to others standing nearby and avoid scenarios where someone was alone, it would be more natural for people to talk to someone on the screen. For example, a person can talk to another person who is at a different charging station through an iPad. Our peers also made comments on how they enjoyed that the “Ice Breaker” idea gave a prompt to break through the awkwardness of starting a conversation. These comments helped us shape the experience for our Speaker and Listener."
     />
     <SingleTitledParagraph
       title="Iterations from Feedback"
@@ -175,7 +173,7 @@ const ConnectPage = ({
     <ParagraphImage
       image={ConnectForget}
       title="Forgetting their Phone"
-      text="All of our participants noted that they completely forgot they were charging their cell phones during the experience. They were so immersed in telling their stories that they forgot about the things around them. Two participants also noted, however, that while they forgot about their phone, charging was still an essential part of the experience that made them stop and interact with the station in the first place."
+      text="All of our participants noted that they completely forgot they were charging their cell phones during the experience. They were so immersed in telling their stories that they forgot about the things around them. Two participants also noted, however, that while they forgot about their phone, charging was still an essential part of the experience. It was the very fact that this was designed as a charging station, that made them stop and interact with it in the first place."
     />
     <SingleTitledParagraph
       title="Digital Realism"
@@ -184,8 +182,8 @@ const ConnectPage = ({
     <ProjectReflections
       projectColor={ConnectColor}
       title="What I learned"
-      text="This quick project made me practice my skills in grace and flexibility. With only one week to create something we pushed ourselves to create the best prototype we could, while still being realistic about  our time constraints and scale."
-      text2="Overall, we were very pleased with how Connect turned out. The whole point of this project was to create human connection through technology and we believe we were successful in doing so. Our participants forgot about their phones and cared more about the connection they were making with themselves and the person on the other side. They were engaged, felt powerful, and ultimately refreshed from the experience. Our Listeners also felt like they gained something from participating. This project showed the power in what speaking and listening can do by connecting two strangers."
+      text="This quick project made me practice my skills in grace and flexibility. With only one week to create something, we pushed ourselves to create the best prototype we could, while still being realistic about our time constraints and scale."
+      text2="Overall, we were very pleased with how Connect turned out. The whole point of this project was to create human connection through technology and we believe we were successful in doing so. Our participants forgot about their phones and cared more about the connection they were making with themselves and the person in the other room. They were engaged, felt powerful, and ultimately refreshed from the experience. Our Listeners also felt like they gained something from participating. This project showed the power in what the simple act of talking and listening can do to connect two strangers."
     />
     <NextProject
       projectKey="astraia"
